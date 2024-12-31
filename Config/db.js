b4d559a9abcc20b58952cfecf350f13b66835632
Krishnadeecodes <163873228+Krishnadeecodes@users.noreply.github.com>
2024-12-31 11:16:49 +0530

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');  // Use destructuring to import Sequelize
 
-const sequelize = new Sequelize('dwin-soft', 'root', 'pass12345', {
+const sequelize = new Sequelize('dwinsoft', 'root', 'pass12345', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,  // Port should be a number, not a string
